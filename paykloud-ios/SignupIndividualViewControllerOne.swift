3eea10d5a32f849fb4caa9878b71fc6adb5bef01
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-03-14 17:11:33 -0400

    add seperate signup process for individuals to paykloud

---
+++ paykloud-ios/SignupIndividualViewControllerOne.swift
@@ -0,0 +1,205 @@
+//
+//  SignupIndividualViewControllerOne.swift
+//  paykloud-ios
+//
+//  Created by Sinan Ulkuatam on 2/19/16.
+//  Copyright © 2016 Sinan Ulkuatam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import TextFieldEffects
+import UIColor_Hex_Swift
+import SVProgressHUD
+
+class SignupIndividualViewControllerOne: UIViewController, UITextFieldDelegate {
+    
+    // WHEN NAVIGATING TO A NAVIGATION CONTROLLER USE SEGUE SHOW NOT MODAL!
+    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    let usernameTextField  = HoshiTextField(frame: CGRect(x: 0, y: 0, width: 300, height: 40))
+    let emailTextField  = HoshiTextField(frame: CGRect(x: 0, y: 0, width: 300, height: 40))
+    
+    // Height not adjusted button bool value
+    var alreadyAdjustedVC2:Bool = false
+    
+    override func viewDidAppear(animated: Bool) {
+        // Focuses view controller on first name text input
+        usernameTextField.becomeFirstResponder()
+        
+        self.continueButton.enabled = false
+        // Allow continue to be clicked
+        Timeout(0.3) {
+            SVProgressHUD.dismiss()
+            self.continueButton.enabled = true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SVProgressHUD.show()
+        
+        let screen = UIScreen.mainScreen().bounds
+        let screenWidth = screen.size.width
+        let screenHeight = screen.size.height
+        
+        // Inherit UITextField Delegate, this is used for next and join on keyboard
+        self.usernameTextField.delegate = self
+        self.emailTextField.delegate = self
+        
+        continueButton.layer.cornerRadius = 5
+        continueButton.backgroundColor = UIColor(rgba: "#1aa8f6")
+        
+        // Programatically set the input fields
+        usernameTextField.tag = 123
+        usernameTextField.textAlignment = NSTextAlignment.Center
+        usernameTextField.borderActiveColor = UIColor(rgba: "#FFF")
+        usernameTextField.borderInactiveColor = UIColor(rgba: "#FFFA") // color with alpha
+        usernameTextField.backgroundColor = UIColor.clearColor()
+        usernameTextField.placeholder = "PayKloud Username"
+        usernameTextField.placeholderColor = UIColor.grayColor()
+        usernameTextField.textColor = UIColor.grayColor()
+        usernameTextField.autocapitalizationType = UITextAutocapitalizationType.None
+        usernameTextField.autocorrectionType = UITextAutocorrectionType.No
+        usernameTextField.keyboardType = UIKeyboardType.Default
+        usernameTextField.returnKeyType = UIReturnKeyType.Next
+        usernameTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
+        usernameTextField.frame.origin.y = screenHeight*0.20 // 25 down from the top
+        usernameTextField.frame.origin.x = (self.view.bounds.size.width - usernameTextField.frame.size.width) / 2.0
+        usernameTextField.returnKeyType = UIReturnKeyType.Next
+        view.addSubview(usernameTextField)
+        
+        emailTextField.tag = 124
+        emailTextField.textAlignment = NSTextAlignment.Center
+        emailTextField.borderActiveColor = UIColor(rgba: "#FFF")
+        emailTextField.borderInactiveColor = UIColor(rgba: "#FFFA") // color with alpha
+        emailTextField.backgroundColor = UIColor.clearColor()
+        emailTextField.placeholder = "Email Address"
+        emailTextField.placeholderColor = UIColor.grayColor()
+        emailTextField.textColor = UIColor.grayColor()
+        emailTextField.autocapitalizationType = UITextAutocapitalizationType.None
+        emailTextField.autocorrectionType = UITextAutocorrectionType.No
+        emailTextField.keyboardType = UIKeyboardType.EmailAddress
+        emailTextField.returnKeyType = UIReturnKeyType.Next
+        emailTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
+        emailTextField.frame.origin.y = screenHeight*0.30 // 25 down from the top
+        emailTextField.frame.origin.x = (self.view.bounds.size.width - emailTextField.frame.size.width) / 2.0
+        emailTextField.returnKeyType = UIReturnKeyType.Next
+        view.addSubview(emailTextField)
+        
+        // Focuses view controller on first name text input
+        usernameTextField.becomeFirstResponder()
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.grayColor()
+        title = ""
+        
+        // Transparent navigation bar
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.translucent = true
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    // Adjusts keyboard height to view
+    func adjustingHeight(show:Bool, notification:NSNotification) {
+        // Check if already adjusted height
+        if(alreadyAdjustedVC2 == false) {
+            var userInfo = notification.userInfo!
+            let keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+            let animationDurarion = userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSTimeInterval
+            let changeInHeight = (CGRectGetHeight(keyboardFrame) + 2) * (show ? 1 : -1)
+            UIView.animateWithDuration(animationDurarion, animations: { () -> Void in
+                self.bottomConstraint.constant += changeInHeight
+            })
+            // Already adjusted height so make it true so it doesn't continue adjusting everytime a label is focused
+            alreadyAdjustedVC2 = true
+        }
+    }
+    
+    func keyboardWillShow(notification:NSNotification) {
+        adjustingHeight(true, notification: notification)
+    }
+    
+    func keyboardWillHide(notification:NSNotification) {
+        adjustingHeight(false, notification: notification)
+    }
+    
+    // Check for valid email
+    func isValidEmail(emailStr:String) -> Bool {
+        // println("validate calendar: \(testStr)")
+        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluateWithObject(emailStr)
+    }
+    
+    // Allow use of next and join on keyboard
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let nextTag: Int = textField.tag + 1
+        // print(nextTag)
+        let nextResponder: UIResponder? = textField.superview?.superview?.viewWithTag(nextTag)
+        if let nextR = nextResponder
+        {
+            // Found next responder, so set it.
+            nextR.becomeFirstResponder()
+        }
+        else
+        {
+            // Not found, so remove keyboard.
+            textField.resignFirstResponder()
+            return true
+        }
+        return false
+    }
+    
+    func isOnlyNumeral(phoneNumber: String) -> Bool {
+        var isValid = true
+        let name = phoneNumber as NSString
+        let nameSet = NSCharacterSet(charactersInString: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLKMNOPQRSTUVWXYZ- ")
+        let set = nameSet.invertedSet
+        let range = name.rangeOfCharacterFromSet(set)
+        let isInvalidName = range.location != NSNotFound
+        if(isInvalidName) {
+            isValid = false
+        }
+        return true
+    }
+    
+    func displayErrorAlertMessage(alertMessage:String) {
+        let displayAlert = UIAlertController(title: "Error", message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        displayAlert.addAction(okAction);
+        self.presentViewController(displayAlert, animated: true, completion: nil);
+    }
+    
+    // VALIDATION
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Username, email, and phone validation
+        if(!isValidEmail(emailTextField.text!)) {
+            displayErrorAlertMessage("Email is not valid")
+        } else if(emailTextField.text?.characters.count < 1 || usernameTextField.text?.characters.count < 1) {
+            displayErrorAlertMessage("Username and email fields cannot be empty")
+        } else {
+            NSUserDefaults.standardUserDefaults().setValue(usernameTextField.text!, forKey: "userUsername")
+            NSUserDefaults.standardUserDefaults().setValue(emailTextField.text!, forKey: "userEmail")
+            NSUserDefaults.standardUserDefaults().synchronize();
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
---
+++ paykloud-ios/SignupIndividualViewControllerThree.swift
@@ -0,0 +1,263 @@
+//
+//  SignupIndividualViewControllerThree.swift
+//  paykloud-ios
+//
+//  Created by Sinan Ulkuatam on 3/14/16.
+//  Copyright © 2016 Sinan Ulkuatam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
+import KeychainSwift
+import TextFieldEffects
+import UIColor_Hex_Swift
+import BEMCheckBox
+import MZAppearance
+import MZFormSheetPresentationController
+
+class SignupIndividualViewControllerThree: UIViewController, UITextFieldDelegate {
+    
+    // WHEN NAVIGATING TO A NAVIGATION CONTROLLER USE SEGUE SHOW NOT MODAL!
+    @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var agreementButton: UIButton!
+    
+    let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+    
+    var switchTermsAndPrivacy: BEMCheckBox = BEMCheckBox(frame: CGRectMake(0, 0, 50, 50))
+    
+    // Keychain
+    let userUsername = NSUserDefaults.standardUserDefaults().stringForKey("userUsername")!
+    let userEmail = NSUserDefaults.standardUserDefaults().objectForKey("userEmail")!
+    let userLegalEntityType = NSUserDefaults.standardUserDefaults().stringForKey("userLegalEntityType")!
+    
+    // Set the locale
+    let countryCode = NSLocale.currentLocale().objectForKey(NSLocaleCountryCode) as! String
+    
+    override func viewDidAppear(animated: Bool) {
+        // Focuses view controller on first name text input
+        //        textField.becomeFirstResponder()
+        
+        self.finishButton.enabled = false
+        // Allow continue to be clicked
+        Timeout(0.3) {
+            SVProgressHUD.dismiss()
+            self.finishButton.enabled = true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SVProgressHUD.show()
+        
+        agreementButton.titleLabel?.textAlignment = NSTextAlignment.Center
+        
+        //        print("user first name", userFirstName)
+        //        print("user last name", userLastName)
+        //        print("user username", userUsername)
+        //        print("user email", userEmail)
+        //        print("user phone", userPhoneNumber)
+        //        print(countryCode)
+        
+        
+        let screen = UIScreen.mainScreen().bounds
+        let screenWidth = screen.size.width
+        let screenHeight = screen.size.height
+        
+        // Set checkbox animation
+        switchTermsAndPrivacy.onAnimationType = BEMAnimationType.OneStroke
+        switchTermsAndPrivacy.offAnimationType = BEMAnimationType.OneStroke
+        switchTermsAndPrivacy.onCheckColor = UIColor(rgba: "#1aa8f6")
+        switchTermsAndPrivacy.onTintColor = UIColor(rgba: "#1aa8f6")
+        switchTermsAndPrivacy.frame.origin.y = screenHeight*0.63 // 75 down from the top
+        switchTermsAndPrivacy.frame.origin.x = (self.view.bounds.size.width - switchTermsAndPrivacy.frame.size.width) / 2.0
+        self.view!.addSubview(switchTermsAndPrivacy)
+        
+        finishButton.layer.cornerRadius = 5
+        finishButton.backgroundColor = UIColor(rgba: "#1aa8f6")
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.grayColor()
+        title = ""
+        
+        // Transparent navigation bar
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.translucent = true
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    @IBAction func finishButtonTapped(sender: AnyObject) {
+        
+        SVProgressHUD.show()
+        
+        if(self.switchTermsAndPrivacy.on.boolValue == false) {
+            // Display error if terms of service and privacy policy not accepted
+            displayErrorAlertMessage("Terms of Service and Privacy Policy were not accepted, could not create account");
+            SVProgressHUD.dismiss()
+            return;
+        }
+        
+        // Set WIFI IP immediately on load using completion handler
+        getWifiAddress { (addr, error) in
+            if addr != nil && self.switchTermsAndPrivacy.on == true {
+                let calcDate = NSDate().timeIntervalSince1970
+                var date: String = "\(calcDate)"
+                
+                var tosContent: [String: AnyObject] = [ "ip": addr!, "date": date ] //also works with [ "model" : NSNull()]
+                var tosJSON: [String: [String: AnyObject]] = [ "data" : tosContent ]
+                let tosNSDict = tosJSON as NSDictionary //no error message
+                
+                let userPassword = KeychainSwift().get("userPassword")!
+                var userDeviceToken: String {
+                    if let userDeviceToken = KeychainSwift().get("user_device_token_ios") {
+                        return userDeviceToken
+                    }
+                    return ""
+                }
+                
+                let parameters : [String : AnyObject] = [
+                    "username":self.userUsername,
+                    "country":self.countryCode,
+                    "email":self.userEmail,
+                    "tos_acceptance" : tosNSDict,
+                    "legal_entity_type": self.userLegalEntityType,
+                    "password":userPassword,
+                    "device_token_ios": userDeviceToken
+                ]
+                Alamofire.request(.POST, apiUrl + "/v1/register", parameters: parameters, encoding:.JSON)
+                    .responseJSON { response in
+                        //print(response.request) // original URL request
+                        //print(response.response?.statusCode) // URL response
+                        //print(response.data) // server data
+                        //print(response.result) // result of response serialization
+                        
+                        if(response.response?.statusCode == 200) {
+                            // Login is successful
+                            NSUserDefaults.standardUserDefaults().setBool(true,forKey:"userLoggedIn");
+                            NSUserDefaults.standardUserDefaults().synchronize();
+                            
+                            print("response 200 success")
+                            // go to main view
+                            self.performSegueWithIdentifier("loginView", sender: self);
+                        } else {
+                            self.displayErrorAlertMessage("Registration Error, username or email already taken.")
+                            print("failed to signup")
+                        }
+                        
+                        switch response.result {
+                        case .Success:
+                            if let value = response.result.value {
+                                let json = JSON(value)
+                                // potentially use completionHandler/closure in future
+                                print("Response: \(json)")
+                                // assign userData to self, access globally
+                                print("register success")
+                                self.displaySuccessAlertMessage("Registration Successful!  You can now login.")
+                            }
+                        case .Failure(let error):
+                            print("failed to signup", error)
+                            self.displayErrorAlertMessage("Registration Error, username or email already taken.")
+                            break
+                        }
+                }
+                
+            } else {
+                SVProgressHUD.dismiss()
+                self.displayErrorAlertMessage("Registration Error, please check your network connection or date/time settings.")
+            }
+            
+        }
+        
+        
+        // TODO: Set keychain username and password
+        
+        SVProgressHUD.dismiss()
+    }
+    
+    func displayErrorAlertMessage(alertMessage:String) {
+        let displayAlert = UIAlertController(title: "Error", message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        displayAlert.addAction(okAction);
+        self.presentViewController(displayAlert, animated: true, completion: nil);
+    }
+    
+    func goToLogin() {
+        self.performSegueWithIdentifier("loginView", sender: self);
+    }
+    
+    func displaySuccessAlertMessage(alertMessage:String) {
+        let displayAlert = UIAlertController(title: "Success", message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: { action in
+            self.goToLogin()
+        })
+        displayAlert.addAction(okAction);
+        self.presentViewController(displayAlert, animated: true, completion: nil);
+    }
+    
+    // Return IP address of WiFi interface (en0) as a String, or `nil`
+    // Used for accepting terms of service
+    func getWifiAddress(completionHandler: (String?, NSError?) -> ()) -> () {
+        var address : String?
+        
+        Alamofire.request(.GET, "https://api.ipify.org").responseString { response in
+            // print(response.request) // original URL request
+            // print(response.response?.statusCode) // URL response
+            // print(response.data) // server data
+            // print(response.result) // result of response serialization
+            
+            switch response.result {
+            case .Success:
+                if let value = response.result.value {
+                    let response = value
+                    // print("SUCCESS! Response: \(response)")
+                    let address = response
+                    completionHandler(address, nil)
+                }
+            case .Failure(let error):
+                completionHandler(nil, error)
+                // print("failed to get IP")
+                // print(error)
+            }
+            
+        }
+        // print("end of func")
+    }
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // Set up modal view for terms and privacy
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            if identifier == "presentModal" {
+                
+                // Initialize and style the terms and conditions modal
+                let presentationSegue = segue as! MZFormSheetPresentationViewControllerSegue
+                presentationSegue.formSheetPresentationController.presentationController?.shouldApplyBackgroundBlurEffect = true
+                presentationSegue.formSheetPresentationController.presentationController?.shouldUseMotionEffect = true
+                presentationSegue.formSheetPresentationController.presentationController?.contentViewSize = CGSizeMake(300, 250)
+                presentationSegue.formSheetPresentationController.presentationController?.containerView?.backgroundColor = UIColor.blackColor()
+                presentationSegue.formSheetPresentationController.presentationController?.containerView?.sizeToFit()
+                presentationSegue.formSheetPresentationController.presentationController?.blurEffectStyle = UIBlurEffectStyle.Dark
+                presentationSegue.formSheetPresentationController.presentationController?.shouldDismissOnBackgroundViewTap = true
+                presentationSegue.formSheetPresentationController.contentViewControllerTransitionStyle = MZFormSheetPresentationTransitionStyle.Fade
+                presentationSegue.formSheetPresentationController.contentViewCornerRadius = 8
+                presentationSegue.formSheetPresentationController.interactivePanGestureDissmisalDirection = .All;
+                
+                // Blur will be applied to all MZFormSheetPresentationControllers by default
+                MZFormSheetPresentationController.appearance().shouldApplyBackgroundBlurEffect = true
+                
+                let navigationController = presentationSegue.formSheetPresentationController.contentViewController as! UINavigationController
+                
+                let presentedViewController = navigationController.viewControllers.first as! PresentedTableViewController
+                presentedViewController.passingString1 = "Terms and Conditions"
+                presentedViewController.passingString2 = "Privacy Policy"
+            }
+        }
+    }
+    
+    
+}
