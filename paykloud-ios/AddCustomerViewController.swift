10b0ecd964b1cbd5bb29db19544086b45dcaae5a
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-03-18 19:09:45 -0400

    update add customer, recurring billing ui with former

--- paykloud-ios/AddCustomerViewController.swift
+++ paykloud-ios/AddCustomerViewController.swift
@@ -9,29 +9,101 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
+import UIKit
+import Former
+import SVProgressHUD
 
-class AddCustomerViewController: UIViewController {
+final class AddCustomerViewController: FormViewController {
     
-    @IBOutlet weak var textFieldCustomerEmail: UITextField!
-    @IBOutlet weak var textFieldCustomerDescription: UITextField!
-    @IBOutlet weak var buttonAddCustomer: UIButton!
+    var dic: Dictionary<String, String> = [:]
+    
+    // MARK: Public
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("view loaded")
-    
-        textFieldCustomerEmail.keyboardType = .EmailAddress
-        //Looks for single or multiple taps.
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        view.addGestureRecognizer(tap)
+        configure()
     }
     
-    @IBAction func addCustomerButtonTapped(sender: AnyObject) {
+    // MARK: Private
+    
+    private func configure() {
+        title = "Add Customer"
+        tableView.contentInset.top = 10
+        tableView.contentInset.bottom = 30
+        tableView.contentOffset.y = -10
+        
+        // screen width and height:
+        let screen = UIScreen.mainScreen().bounds
+        let screenWidth = screen.size.width
+        let screenHeight = screen.size.height
+        
+        // UI
+        let addCustomerButton = UIButton(frame: CGRect(x: 0, y: screenHeight*0.91, width: screenWidth, height: 60.0))
+        addCustomerButton.backgroundColor = UIColor(rgba: "#1796fa")
+        addCustomerButton.tintColor = UIColor(rgba: "#fff")
+        addCustomerButton.setTitleColor(UIColor(rgba: "#fff"), forState: .Normal)
+        addCustomerButton.titleLabel?.font = UIFont(name: "Nunito-SemiBold", size: 16)
+        addCustomerButton.setTitle("Add Customer", forState: .Normal)
+        addCustomerButton.layer.cornerRadius = 0
+        addCustomerButton.layer.masksToBounds = true
+        addCustomerButton.clipsToBounds = true
+        addCustomerButton.addTarget(self, action: "addCustomerButtonTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(addCustomerButton)
+        
+        self.navigationItem.title = "Add Customer"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.darkGrayColor()]
+        
+        // Create RowFomers
+        
+        let emailRow = TextFieldRowFormer<FormTextFieldCell>() {
+//            $0.textField.textColor = .formerColor()
+            $0.textField.font = .systemFontOfSize(15)
+            $0.textField.autocapitalizationType = .None
+            $0.textField.autocorrectionType = .No
+            $0.textField.keyboardType = .EmailAddress
+            }.configure {
+                $0.placeholder = "Customer email"
+            }.onTextChanged { [weak self] in
+                self?.dic["customerEmailKey"] = $0
+        }
+
+
+        let descriptionRow = TextViewRowFormer<FormTextViewCell>() {
+            $0.textView.font = .systemFontOfSize(15)
+            }.configure {
+                $0.placeholder = "Customer Description"
+                $0.rowHeight = 150
+            }.onTextChanged { [weak self] in
+                self?.dic["customerDescriptionKey"] = $0
+        }
+        
+        // Create Headers
+        
+        let createHeader: (() -> ViewFormer) = {
+            return CustomViewFormer<FormHeaderFooterView>()
+                .configure {
+                    $0.viewHeight = 20
+            }
+        }
+        
+        // Create SectionFormers
+        
+        let titleSection = SectionFormer(rowFormer: emailRow, descriptionRow)
+            .set(headerViewFormer: createHeader())
+        
+        former.append(sectionFormer: titleSection)
+    }
+    
+    func addCustomerButtonTapped(sender: AnyObject) {
+        
+        SVProgressHUD.show()
+        
+        print(dic)
         
         print("add customer tapped")
         // Post plan using Alamofire
-        let cust_email = textFieldCustomerEmail.text! as String
-        let cust_desc = textFieldCustomerDescription.text! as String
+        let cust_email = dic["customerEmailKey"]
+        let cust_desc = dic["customerDescriptionKey"]
         
         let stripeKey = userData!["user"]["stripe"]["secretKey"].stringValue
         
@@ -41,8 +113,8 @@ class AddCustomerViewController: UIViewController {
             "Content-Type": "application/x-www-form-urlencoded"
         ]
         let parameters : [String : AnyObject] = [
-            "email": cust_email,
-            "description": cust_desc,
+            "email": cust_email!,
+            "description": cust_desc! ?? "",
         ]
         
         Alamofire.request(.POST, stripeApiUrl + "/v1/customers",
@@ -65,22 +137,23 @@ class AddCustomerViewController: UIViewController {
                 switch response.result {
                 case .Success:
                     if let value = response.result.value {
+                        SVProgressHUD.showSuccessWithStatus("Customer Added!")
                         let json = JSON(value)
                         print(json)
                         self.dismissKeyboard()
                         
                     }
                 case .Failure(let error):
+                    SVProgressHUD.showErrorWithStatus("Error Adding Customer")
                     print(error)
                 }
         }
         
     }
-
+    
     //Calls this function when the tap is recognized.
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
-    
 }
--- paykloud-ios/RecurringBillingPlanViewController.swift
+++ paykloud-ios/RecurringBillingPlanViewController.swift
@@ -1,99 +1,207 @@
 //
-//  RecurringBillingPlanViewController.swift
+//  AddCustomerViewController.swift
 //  paykloud-ios
 //
-//  Created by Sinan Ulkuatam on 3/14/16.
+//  Created by Sinan Ulkuatam on 3/15/16.
 //  Copyright © 2016 Sinan Ulkuatam. All rights reserved.
 //
 
 import Foundation
 import Alamofire
 import SwiftyJSON
+import UIKit
+import Former
+import SVProgressHUD
 
-class RecurringBillingViewController: UIViewController {
+final class RecurringBillingViewController: FormViewController {
     
-    @IBOutlet weak var textFieldPlanID: UITextField!
-    @IBOutlet weak var textFieldPlanName: UITextField!
-    @IBOutlet weak var textFieldPlanCurrency: UITextField!
-    @IBOutlet weak var textFieldPlanAmount: UITextField!
-    @IBOutlet weak var textFieldPlanInterval: UITextField!
-    @IBOutlet weak var textFieldPlanTrialPeriod: UITextField!
-    @IBOutlet weak var textFieldPlanStatementDescription: UITextField!
-    @IBOutlet weak var buttonAddPlan: UIButton!
-    @IBOutlet weak var buttonReturnHome: UIButton!
+    var dic: Dictionary<String, String> = [:]
     
-    @IBAction func addPlanButtonTapped(sender: AnyObject) {
+    // MARK: Public
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    // MARK: Private
+    
+    private func configure() {
+        title = "Add Customer"
+        tableView.contentInset.top = 10
+        tableView.contentInset.bottom = 30
+        tableView.contentOffset.y = -10
+        
+        // screen width and height:
+        let screen = UIScreen.mainScreen().bounds
+        let screenWidth = screen.size.width
+        let screenHeight = screen.size.height
+        
+        // UI
+        let addPlanButton = UIButton(frame: CGRect(x: 0, y: screenHeight*0.91, width: screenWidth, height: 60.0))
+        addPlanButton.backgroundColor = UIColor(rgba: "#1796fa")
+        addPlanButton.tintColor = UIColor(rgba: "#fff")
+        addPlanButton.setTitleColor(UIColor(rgba: "#fff"), forState: .Normal)
+        addPlanButton.titleLabel?.font = UIFont(name: "Nunito-SemiBold", size: 16)
+        addPlanButton.setTitle("Add Plan", forState: .Normal)
+        addPlanButton.layer.cornerRadius = 0
+        addPlanButton.layer.masksToBounds = true
+        addPlanButton.clipsToBounds = true
+        addPlanButton.addTarget(self, action: "addPlanButtonTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(addPlanButton)
+        
+        self.navigationItem.title = "Add Plan"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.darkGrayColor()]
+        
+        // Create RowFomers
+        
+        let planIdRow = TextFieldRowFormer<FormTextFieldCell>() {
+            //            $0.textField.textColor = .formerColor()
+            $0.textField.font = .systemFontOfSize(15)
+            }.configure {
+                $0.placeholder = "Plan ID (e.g. gold)"
+            }.onTextChanged { [weak self] in
+                self?.dic["planIdKey"] = $0
+        }
+        
+        let planNameRow = TextFieldRowFormer<FormTextFieldCell>() {
+            //            $0.textField.textColor = .formerColor()
+            $0.textField.font = .systemFontOfSize(15)
+            }.configure {
+                $0.placeholder = "Plan Name (e.g. Gold)"
+            }.onTextChanged { [weak self] in
+                self?.dic["planNameKey"] = $0
+        }
+        
+        let planCurrencyRow = TextFieldRowFormer<FormTextFieldCell>() {
+            //            $0.textField.textColor = .formerColor()
+            $0.textField.font = .systemFontOfSize(15)
+            }.configure {
+                $0.placeholder = "Currency"
+            }.onTextChanged { [weak self] in
+                self?.dic["planCurrencyKey"] = $0
+        }
+        
+        let planAmountRow = TextFieldRowFormer<FormTextFieldCell>() {
+            //            $0.textField.textColor = .formerColor()
+            $0.textField.font = .systemFontOfSize(15)
+            }.configure {
+                $0.placeholder = "Amount"
+            }.onTextChanged { [weak self] in
+                self?.dic["planAmountKey"] = $0
+        }
+        
+        let planIntervalRow = TextFieldRowFormer<FormTextFieldCell>() {
+            //            $0.textField.textColor = .formerColor()
+            $0.textField.font = .systemFontOfSize(15)
+            }.configure {
+                $0.placeholder = "Interval"
+            }.onTextChanged { [weak self] in
+                self?.dic["planIntervalKey"] = $0
+        }
+        
+        let planTrialPeriodRow = TextFieldRowFormer<FormTextFieldCell>() {
+            //            $0.textField.textColor = .formerColor()
+            $0.textField.font = .systemFontOfSize(15)
+            }.configure {
+                $0.placeholder = "Trial Period"
+            }.onTextChanged { [weak self] in
+                self?.dic["planTrialPeriodKey"] = $0
+        }
+        
+        let planStatementDescriptionRow = TextFieldRowFormer<FormTextFieldCell>() {
+            //            $0.textField.textColor = .formerColor()
+            $0.textField.font = .systemFontOfSize(15)
+            }.configure {
+                $0.placeholder = "Statement Description"
+            }.onTextChanged { [weak self] in
+                self?.dic["planStatementDescriptionKey"] = $0
+        }
+        
+        // Create Headers
+        
+        let createHeader: (() -> ViewFormer) = {
+            return CustomViewFormer<FormHeaderFooterView>()
+                .configure {
+                    $0.viewHeight = 20
+            }
+        }
+        
+        // Create SectionFormers
+        
+        let titleSection = SectionFormer(rowFormer: planIdRow, planNameRow, planCurrencyRow, planAmountRow, planIntervalRow, planTrialPeriodRow, planStatementDescriptionRow)
+            .set(headerViewFormer: createHeader())
+        
+        former.append(sectionFormer: titleSection)
+    }
+    
+    func addPlanButtonTapped(sender: AnyObject) {
+        
+        SVProgressHUD.show()
+        
         print("add plan tapped")
         // Post plan using Alamofire
-        let plan_id = textFieldPlanID.text! as String
-        let plan_name = textFieldPlanName.text! as String
-        let plan_currency = textFieldPlanCurrency.text! as String
-        let plan_amount = textFieldPlanAmount.text!
-        let plan_interval = textFieldPlanInterval.text! as String
-//        print(textFieldPlanTrialPeriod.text!)
-//        print(textFieldPlanStatementDescription.text!)
+        let plan_id = dic["planIdKey"]
+        let plan_name = dic["planNameKey"]
+        let plan_currency = dic["planCurrencyKey"]
+        let plan_amount = dic["planAmountKey"]
+        let plan_interval = dic["planIntervalKey"]
+        let plan_trial_period = dic["planTrialPeriodKey"]
+        let plan_statement_desc = dic["planStatementDescriptionKey"]
         
         print("plan id is", plan_id)
         let stripeKey = userData!["user"]["stripe"]["secretKey"].stringValue
-
+        
         let headers = [
             "Authorization": "Bearer " + stripeKey,
             "Content-Type": "application/x-www-form-urlencoded"
         ]
         let parameters : [String : AnyObject] = [
-            "id": plan_id,
-            "amount": plan_amount,
-            "interval": plan_interval,
-            "name": plan_name,
-            "currency": plan_currency
+            "id": plan_id!,
+            "amount": plan_amount!,
+            "interval": plan_interval!,
+            "name": plan_name!,
+            "currency": plan_currency!
         ]
         
         Alamofire.request(.POST, stripeApiUrl + "/v1/plans",
-        parameters: parameters,
-        encoding:.URL,
-        headers: headers)
-        .responseJSON { response in
-            print(response.request) // original URL request
-            print(response.response?.statusCode) // URL response
-            print(response.data) // server data
-            print(response.result) // result of response serialization
-            
-            // go to main view
-            if(response.response?.statusCode == 200) {
-                print("green light")
-            } else {
-                print("red light")
-            }
-            
-            switch response.result {
-            case .Success:
-                if let value = response.result.value {
-                    let json = JSON(value)
-                    print(json)
-                    self.dismissKeyboard()
-                    
+            parameters: parameters,
+            encoding:.URL,
+            headers: headers)
+            .responseJSON { response in
+                print(response.request) // original URL request
+                print(response.response?.statusCode) // URL response
+                print(response.data) // server data
+                print(response.result) // result of response serialization
+                
+                // go to main view
+                if(response.response?.statusCode == 200) {
+                    print("green light")
+                } else {
+                    print("red light")
                 }
-            case .Failure(let error):
-                print(error)
-            }
+                
+                switch response.result {
+                case .Success:
+                    if let value = response.result.value {
+                        SVProgressHUD.showSuccessWithStatus("Plan added!")
+                        let json = JSON(value)
+                        print(json)
+                        self.dismissKeyboard()
+                        
+                    }
+                case .Failure(let error):
+                    SVProgressHUD.showErrorWithStatus("Error adding plan")
+                    print(error)
+                }
         }
-
+        
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("view loaded")
-        
-        //Looks for single or multiple taps.
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        view.addGestureRecognizer(tap)
-    }
-
+    
     //Calls this function when the tap is recognized.
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
-    
 }
