0a615c0f680c56abb4a2bf38e1d666de36a2acbc
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-03-24 11:17:46 -0400

    rename - refactor

---
+++ app-ios/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  protonpay-ios
+//
+//  Created by Sinan Ulkuatam on 3/23/16.
+//  Copyright © 2016 Sinan Ulkuatam. All rights reserved.
+//
+
+import Foundation
+
+class Secrets {
+    class var PlaidClientId: String { return "PLAID_CLIENT_ID" }
+    class var PlaidSecret: String { return "PLAID_SECRET" }
+}
---
+++ app-iosTests/paykloud_iosTests.swift
@@ -0,0 +1,36 @@
+////
+////  protonpay_iosTests.swift
+////  protonpay-iosTests
+////
+////  Created by Sinan Ulkuatam on 2/9/16.
+////  Copyright © 2016 Sinan Ulkuatam. All rights reserved.
+////
+//
+//import XCTest
+//@testable import paykloud_ios
+//
+//class paykloud_iosTests: XCTestCase {
+//    
+//    override func setUp() {
+//        super.setUp()
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//    
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        super.tearDown()
+//    }
+//    
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+//    
+//}
