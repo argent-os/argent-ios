0c9b67db6ae864bfca0ba38b5931531c2199e9f3
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-04-02 08:59:11 -0400

    add user notifications page :)

---
+++ app-ios/Notification.swift
@@ -0,0 +1,85 @@
+//
+//  Notification.swift
+//  protonpay-ios
+//
+//  Created by Sinan Ulkuatam on 4/1/16.
+//  Copyright © 2016 Sinan Ulkuatam. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+class NotificationItem {
+    
+    let id: String
+    let text: String
+    let date: String
+    let uid: String
+    
+    required init(id: String, text: String, date: String, uid: String) {
+        self.id = text
+        self.text = text
+        self.date = text
+        self.uid = text
+    }
+    
+    class func endpointForNotifications() -> String {
+        let endpoint = "http://192.168.1.232:5001/v1/notification/"
+        return endpoint
+    }
+    
+    class func getNotificationList(completionHandler: ([NotificationItem]?, NSError?) -> Void) {
+        Alamofire.request(.GET, self.endpointForNotifications())
+            .responseNotificationsItemsArray { response in
+                print(response)
+                print("inside completion handler callback")
+                completionHandler(response.result.value, response.result.error)
+        }
+    }
+}
+
+extension Alamofire.Request {
+    func responseNotificationsItemsArray(completionHandler: Response<[NotificationItem], NSError> -> Void) -> Self {
+        let serializer = ResponseSerializer<[NotificationItem], NSError> { request, response, data, error in
+            guard let responseData = data else {
+                let failureReason = "Image URL could not be serialized because input data was nil."
+                let error = Error.errorWithCode(.DataSerializationFailed, failureReason: failureReason)
+                return .Failure(error)
+            }
+            
+            let JSONResponseSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
+            let result = JSONResponseSerializer.serializeResponse(request, response, responseData, error)
+            
+            switch result {
+            case .Success(let value):
+                let json = JSON(value)
+                print(json)
+                guard json.error == nil else {
+                    print(json.error!)
+                    return .Failure(json.error!)
+                }
+                
+                var itemsArray = [NotificationItem]()
+                let notifications = json.arrayValue
+                print(notifications)
+                for jsonItem in notifications {
+                    let text = jsonItem["text"].stringValue
+                    let id = jsonItem["_id"].stringValue
+                    let uid = jsonItem["user_id"].stringValue
+                    let date = jsonItem["date"].stringValue
+                    let item = NotificationItem(id: id, text: text, date: date, uid: uid)
+                    print(text)
+                    print(item)
+                    itemsArray.append(item)
+                }
+                
+                return .Success(itemsArray)
+            case .Failure(let error):
+                return .Failure(error)
+            }
+        }
+        
+        return response(responseSerializer: serializer, completionHandler: completionHandler)
+    }
+}
--- app-ios/NotificationViewController.swift
+++ app-ios/NotificationViewController.swift
@@ -11,18 +11,18 @@ import Alamofire
 import SwiftyJSON
 import SVProgressHUD
 
-enum StockType {
-    case Tech
-    case Cars
-    case Telecom
-}
+//enum StockType {
+//    case Tech
+//    case Cars
+//    case Telecom
+//}
 
 class NotificationsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var itemsArray:Array<StockQuoteItem>?
+    var itemsArray:Array<NotificationItem>?
     @IBOutlet var tableView: UITableView?
     
-    var stockType: StockType = .Tech
+//    var stockType: StockType = .Tech
     
     var refreshControl = UIRefreshControl()
     var dateFormatter = NSDateFormatter()
@@ -41,44 +41,44 @@ class NotificationsViewController: UIViewController, UITableViewDataSource, UITa
         self.refreshControl.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
         self.tableView?.addSubview(refreshControl)
         
-        self.loadStockQuoteItems()
+        self.loadNotificationItems()
     }
     
-    func symbolsStringForCurrentStockType() -> Array<String>
-    {
-        switch self.stockType {
-        case .Tech:
-            return ["AAPL", "GOOG", "YHOO"]
-        case .Cars:
-            return ["GM", "F"]
-        case .Telecom:
-            return ["T", "VZ", "CMCSA"]
-        }
-    }
+//    func symbolsStringForCurrentStockType() -> Array<String>
+//    {
+//        switch self.stockType {
+//        case .Tech:
+//            return ["AAPL", "GOOG", "YHOO"]
+//        case .Cars:
+//            return ["GM", "F"]
+//        case .Telecom:
+//            return ["T", "VZ", "CMCSA"]
+//        }
+//    }
+
+//    @IBAction func stockTypeSegmentedControlValueChanged(sender: UISegmentedControl)
+//    {
+//        switch sender.selectedSegmentIndex {
+//        case 0:
+//            self.stockType = .Tech
+//        case 1:
+//            self.stockType = .Cars
+//        case 2:
+//            self.stockType = .Telecom
+//        default:
+//            print("Segment index out of known range, do you need to add to the enum or switch statement?")
+//        }
+//        
+//        // load data for our new symbols
+//        refresh(sender)
+//    }
     
-    @IBAction func stockTypeSegmentedControlValueChanged(sender: UISegmentedControl)
-    {
-        switch sender.selectedSegmentIndex {
-        case 0:
-            self.stockType = .Tech
-        case 1:
-            self.stockType = .Cars
-        case 2:
-            self.stockType = .Telecom
-        default:
-            print("Segment index out of known range, do you need to add to the enum or switch statement?")
-        }
-        
-        // load data for our new symbols
-        refresh(sender)
-    }
-    
-    func loadStockQuoteItems() {
-        let symbols = symbolsStringForCurrentStockType()
-        StockQuoteItem.getFeedItems(symbols, completionHandler: { (items, error) in
+    func loadNotificationItems() {
+//        let symbols = symbolsStringForCurrentStockType()
+        NotificationItem.getNotificationList({ (items, error) in
             if error != nil
             {
-                let alert = UIAlertController(title: "Error", message: "Could not load stock quotes :( \(error?.localizedDescription)", preferredStyle: UIAlertControllerStyle.Alert)
+                let alert = UIAlertController(title: "Error", message: "Could not load notifications \(error?.localizedDescription)", preferredStyle: UIAlertControllerStyle.Alert)
                 alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
                 self.presentViewController(alert, animated: true, completion: nil)
             }
@@ -100,7 +100,7 @@ class NotificationsViewController: UIViewController, UITableViewDataSource, UITa
     
     func refresh(sender:AnyObject)
     {
-        self.loadStockQuoteItems()
+        self.loadNotificationItems()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -112,13 +112,14 @@ class NotificationsViewController: UIViewController, UITableViewDataSource, UITa
         let item = self.itemsArray?[indexPath.row]
         cell.textLabel?.text = ""
         cell.detailTextLabel?.text = ""
-        if let symbol = item?.symbol, ask = item?.ask
+        print(item)
+        if let text = item?.text, date = item?.date
         {
-            cell.textLabel?.text = symbol + " @ $" + ask
+            cell.textLabel?.text = text + " on date " + date
         }
-        if let low = item?.yearLow, high = item?.yearHigh
+        if let id = item?.id, uid = item?.uid
         {
-            cell.detailTextLabel?.text = "Year: " + low + " - " + high
+            cell.detailTextLabel?.text = "id: " + id + " / uid " + uid
         }
         return cell
     }
