f5f18278079b1ccb14cefac067ab87a0c6a77c31
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-05-19 15:55:36 -0400

    add individual and company segues at signup progromatically and on storyboard

--- app-ios/LoginViewController.swift
+++ app-ios/LoginViewController.swift
@@ -57,7 +57,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate  {
         let containerFrame: CGRect = self.loginBox.frame
         loginBox.frame = containerFrame
         loginBox.layer.cornerRadius = 5
-        loginBox.layer.borderColor = UIColor(rgba: "#fff5").CGColor
+        loginBox.layer.borderColor = UIColor.whiteColor().colorWithAlphaComponent(0.3).CGColor
         loginBox.layer.borderWidth = 1
         loginBox.layer.masksToBounds = true
         
