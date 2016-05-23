931df562f2ba1482553f53a7fa24aec297bf79cc
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-05-22 22:33:43 -0400

    save state tabbar

--- app-ios/AppDelegate.swift
+++ app-ios/AppDelegate.swift
@@ -127,9 +127,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TRTabBarControllerDelegat
         
         // Initialize Plaid, change to .Production before golive
         Plaid.sharedInstance().setPublicKey("fb32b0520292ad69be7b4d1ade4bd3")
-
-        // Display PasscodeLock on Launch
-        passcodeLockPresenter.presentPasscodeLock()
         
         // Globally dark keyboard
         UITextField.appearance().keyboardAppearance = .Light
@@ -199,30 +196,58 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TRTabBarControllerDelegat
         KeychainSwift().set(deviceTokenString, forKey: "deviceToken")
         // print( deviceTokenString )
     }
-    
+
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-        print("applicationWillResignActive") //ignore        
+        print("applicationWillResignActive") //ignore  
+        print(application)
+        // Display PasscodeLock on Launch
+        passcodeLockPresenter.presentPasscodeLock()
+        //self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // Global background color
+        //self.window!.makeKeyAndVisible()
+        //self.window!.makeKeyWindow()
+        //let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("RootViewController")
+        //self.window!.rootViewController = viewController
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        print("applicationDidEnterBackground") //ignore
-        print(application)
+        /*
+         Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+         If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+         */
+        //Remember the users last tab selection
+        if let tabBarController = window?.rootViewController as? UITabBarController {
+            let tabIndex: Int = tabBarController.selectedIndex
+            let userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+            userDefaults.setInteger(tabIndex, forKey: "activeTab")
+            print(userDefaults)
+            if !userDefaults.synchronize() {
+                print("Error Synchronizing NSUserDefaults")
+            }
+        }
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        /*
+         Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+         */
+        //Set the tabBarController to the last visted tab
+        if let tabBarController = window?.rootViewController as? UITabBarController {
+            guard let activeTab = NSUserDefaults.standardUserDefaults().valueForKey("activeTab") else {
+                return
+            }
+            // Display PasscodeLock on Launch
+            passcodeLockPresenter.presentPasscodeLock()
+            tabBarController.selectedIndex = Int(activeTab as! NSNumber)
+        }
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         print("applicationWillEnterForeground")
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        // Set push notification badge to zero
-        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
-        print("applicationDidBecomeActive")
+        print(application)
     }
 
     func applicationWillTerminate(application: UIApplication) {
